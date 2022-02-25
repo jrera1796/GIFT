35c015df2cdb530d55e27bb23dbbbbf244b61129
Ricky Leung <ricxx0320@gmail.com>
2022-02-25 00:57:19 -0800

    modified footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container">
-        &copy;{new Date().getFullYear()} by Lernantino
+        &copy;{new Date().getFullYear()} by
+        <a href="https://github.com/kcheykim"> Kosal Cheykim</a>,
+        <a href="https://github.com/mockcomic"> Kurtis Hight</a>,
+        <a href="https://github.com/jrera1796"> Jose Rivera</a>,
+        <a href="https://github.com/ricky0320"> Ching Leung</a>
       </div>
     </footer>
   );
