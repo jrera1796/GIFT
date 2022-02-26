b6a4738ef1afb86ca228807e09c17f5fff0c8b2a
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-02-26 01:36:34 -0800

    search page added and styled a bit

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -24,7 +24,7 @@ const Header = () => {
           ) : (
             <>
               <Link to="/login">Login/Signup</Link>
-              <Link to="/giftsideas">GiftIdeas</Link>
+              <Link to="/search">GiftIdeas</Link>
             </>
           )}
         </nav>
