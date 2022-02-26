9ea4f679f5ba7477a5cfd36ee3470e4770fabfd9
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-02-25 18:59:40 -0800

    auto scrolling hero, changes to Home.js

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Hero = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const array = ["present1", "valentines1", "christmas1"];
+  // const [seconds, setSeconds] = useState(0);
+
+  const array = ["present1", "valentines1", "christmas1", "restaurant1"];
+  
   function updateIndex(newIndex) {
     if (newIndex < 0) {
       newIndex = array.length - 1;
@@ -12,15 +15,22 @@ const Hero = () => {
     setActiveIndex(newIndex)
   };
 
+  useEffect(() => {
+    const next = (activeIndex + 1) % array.length;
+    const id = setTimeout(() => setActiveIndex(next), 15000)
+    return () => clearTimeout(id);
+  })
+
   return (
     <div className="carousel" data-carousel>
-      <button className="carousel-button prev" data-carousel-button="prev" onClick={() => { updateIndex(activeIndex - 1) }}>&#8656;</button>
-      <button className="carousel-button next" data-carousel-button="next" onClick={() => { updateIndex(activeIndex + 1) }}>&#8658;</button>
-      <ul data-slides>
-        <li>
+   
+      <button className="carousel-button prev" data-carousel-button="prev" onClick={() => { updateIndex(activeIndex - 1) }}></button>
+      <button className="carousel-button next" data-carousel-button="next" onClick={() => { updateIndex(activeIndex + 1) }}></button>
+      
           <img alt={array[activeIndex]} src={require(`../../assets/images/${array[activeIndex]}.jpeg`)}></img>
-        </li>
-      </ul>
+       
+      
+   
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,6 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div><Hero></Hero></div>
+    <Hero></Hero>
   )
 }
