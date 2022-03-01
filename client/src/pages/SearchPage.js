21bbb746d71f85c202204cee3578b7845b738332
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-02-28 21:43:05 -0800

    Search with personaility results

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -2,38 +2,43 @@ import React, { useState, useEffect } from 'react';
 import searchProducts from '../utils/searchAPI';
 import Auth from '../utils/auth';
 import 'bulma/css/bulma.css';
+import {
+
+
+  useParams
+} from "react-router-dom";
 import { saveGiftIds, getSavedGiftIds } from '../utils/savingGifts';
 import { useMutation } from '@apollo/client';
 import { SAVE_GIFT } from '../utils/mutations';
 
-// const categories = [
-//   { 'Apps & Games': 2350150011 },
-//   { 'Arts, Crafts & Sewing': 2617942011 },
-//   { 'Beauty': 11055981 },
-//   { 'Books': 1000 },
-//   { 'CDs & Vinyl': 301668 },
-//   { 'Clothing, Shoes & Jewelry': 7141124011 },
-//   { 'Collectibles & Fine Arts': 4991426011 },
-//   { 'Digital Music': 624868011 },
-//   { 'Electronics': 493964 },
-//   { 'Gift Cards': 2864120011 },
-//   { 'Grocery & Gourmet Food': 16310211 },
-//   { 'Handmade': 11260433011 },
-//   { 'Health & Personal Care': 3760931 },
-//   { 'Home & Kitchen': 1063498 },
-//   { 'Industrial & Scientific': 16310161 },
-//   { 'Movies & TV': 2625374011 },
-//   { 'Musical Instruments': 11965861 },
-//   { 'Patio, Lawn & Garden': 3238155011 },
-//   { 'Pet Supplies': 2619534011 },
-//   { 'Software': 409488 },
-//   { 'Sports & Outdoors': 3375301 },
-//   { 'Tools & Home Improvement': 468240 },
-//   { 'Toys & Games': 165795011 },
-//   { 'Vehicles': 10677470011 },
-//   { 'Video Games': 11846801 }
+const categories = [
+  { "Apps,Games": 2350150011 },
+  { "Arts, Crafts, Sewing": 2617942011 },
+  { "Beauty": 11055981 },
+  { "Books": 1000 },
+  { "CDs, Vinyl": 301668 },
+  { "Clothing, Shoes, Jewelry": 7141124011 },
+  { "Collectibles, Fine Arts": 4991426011 },
+  { "Digital Music": 624868011 },
+  { "Electronics": 493964 },
+  { "Gift Cards": 2864120011 },
+  { "Grocery, Gourmet Food": 16310211 },
+  { "Handmade": 11260433011 },
+  { "Health, Personal Care": 3760931 },
+  { "Home, Kitchen": 1063498 },
+  { "Industrial, Scientific": 16310161 },
+  { "Movies, TV": 2625374011 },
+  { "Musical Instruments": 11965861 },
+  { "Patio, Lawn, Garden": 3238155011 },
+  { "Pet Supplies": 2619534011 },
+  { "Software": 409488 },
+  { "Sports, Outdoors": 3375301 },
+  { "Tools, Home Improvement": 468240 },
+  { "Toys, Games": 165795011 },
+  { "Vehicles": 10677470011 },
+  { "Video Games": 11846801 }
 
-// ]
+]
 
 const SearchPage = () => {
 
@@ -48,8 +53,8 @@ const SearchPage = () => {
     }
   }
 
-  function searchMore(keywords, category) { return searchProducts(keywords, category) };
-  const [searchGift, setSearchGift] = useState('');
+  // function searchMore(keywords, category) { return searchProducts(keywords, category) };
+  const [searchGift, setSearchGift] = useState('cat');
   const [searchGiftCategory, setSearchGiftCategory] = useState(1000);
   const [searchedData, setSearchedData] = useState([]);
 
@@ -63,13 +68,13 @@ const SearchPage = () => {
     event.preventDefault();
 
     try {
+      setShowResults(false)
       setToggleLoading(true); loadingScreen()
-      const data = await searchMore(searchGift, searchGiftCategory);
+      const data = await searchProducts(searchGift, searchGiftCategory);
       setShowResults(true);
       setSearchedData(data);
       setSearchGift('');
       setSearchGiftCategory(1000)
-      console.log(searchedData)
     }
     catch (error) {
       console.log(error)
@@ -82,18 +87,32 @@ const SearchPage = () => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     if (!token) { return false; }
     try {
-      const {resData} = await saveGift({ variables: { data: { ...giftToSave}}});
+      const { resData } = await saveGift({ variables: { data: { ...giftToSave } } });
       console.log(resData);
       setSavedGiftIds([...savedGiftIds, giftToSave.giftId]);
-    } catch (e) {console.log('Cannot Save Gift'); }
+    } catch (e) { console.log('Cannot Save Gift'); }
   }
 
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  let value = params.personality;
-  console.log(value)
-  
+
+
+  useEffect(() => {
+    let params = (new URL(document.location)).searchParams
+    let personality = params.get('personality');
+
+    async function fetchData() {
+      if (personality === 'INFJ') {
+        setToggleLoading(true); loadingScreen();
+        const prepopulateData = await searchProducts('Advocacy', 1000);
+        console.log(searchedData)
+        setSearchedData(prepopulateData);
+        setShowResults(true);
+      } else {
+        console.log('Not' + params)
+      }
+    } fetchData()
+  }, []);
+
+
   return (
 
     <div>
@@ -104,7 +123,6 @@ const SearchPage = () => {
               <input
                 className='input is-normal'
                 name='searchGift'
-                value={searchGift}
                 onChange={(e) => setSearchGift(e.target.value)}
                 type='text'
                 placeholder='Search for gifts'
@@ -118,10 +136,10 @@ const SearchPage = () => {
                 type='select'
                 placeholder='Search by Category'
               >
-                <option defaultValue={1000}>Search by category</option>
-                {/* {categories.map((c) => (
-                  <option key={c.value} value={value}>{key}</option>
-                ))} */}
+                <option value={1000}>Search by category</option>
+                {categories.map((c) => (
+                  <option key={JSON.stringify(c)} value={c}>{JSON.stringify(c)}</option>
+                ))}
                 {/* Can't get this to display */}
               </select>
             </div>
@@ -155,12 +173,12 @@ const SearchPage = () => {
                     </span>
                   </p>
                   {Auth.loggedIn() ? (<p className="card-footer-item">
-                    <button 
-                    className='button is-medium'
-                    disabled={savedGiftIds?.some((savedId) => savedId === netData.giftId)}
-                    onClick= {() => handleSaveGift(netData.giftId)}>
+                    <button
+                      className='button is-medium'
+                      disabled={savedGiftIds?.some((savedId) => savedId === netData.giftId)}
+                      onClick={() => handleSaveGift(netData.giftId)}>
                       {savedGiftIds?.some((savedId) => savedId === netData.giftId)
-                      ? 'Save' : 'Already Saved'}
+                        ? 'Save' : 'Already Saved'}
                     </button>
                   </p>
                   ) : (
