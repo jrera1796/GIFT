622d7fde11a77a0470ca4c850fba9b1d9130f442
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-03-02 13:02:34 -0800

    toggle login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import SignupForm from '../components/SignupForm';
 
+
 export default function Login() {
+	const [toggle, setToggle] = useState()
   return (
 	<div>
-		<LoginForm />
-		<SignupForm />
+		{toggle?(
+
+		<div className='box'><button className='is-button' onClick={() => setToggle(false)}>SignUp</button><LoginForm /></div>) 
+		:(
+			<div className='box'><button className='is-button' onClick={() => setToggle(true)}>Login</button><SignupForm/></div>
+		)
+		}
 	</div>
 
   )
