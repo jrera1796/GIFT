23b4273c3c3839b0142445d202e3b48df7e550df
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-03-01 23:30:44 -0800

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
-// //Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+
+//Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
