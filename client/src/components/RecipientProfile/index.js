49c11115a2621b31582151e4aea7c8172b50af3e
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-03-02 19:26:06 -0800

    recipient profile

---
+++ client/src/components/RecipientProfile/index.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import { GET_RECIPIENTS } from '../../utils/queries'
+// import { useQuery } from '@apollo/client';
+
+
+// const RecipientProfile = (recipientID) => {
+// const data = useQuery(GET_RECIPIENTS)
+// const recipient = data
+// console.log(recipient, '\n Line Break', data)
+// return (<div>
+// {recipient.savedGifts.map(rec => {
+//   <div>{rec.title}</div>
+// })}
+// </div>
+// )
+// }
+
+// export default RecipientProfile;
