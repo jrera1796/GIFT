a6b32684cb22946464e9b2af5748d0fe71fb43e0
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-03-02 20:56:30 -0800

    debugging heroku

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useParams } from 'react';
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 import searchProducts from '../utils/searchAPI';
 import Auth from '../utils/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
