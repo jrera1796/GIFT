bd8f10ede567c8cf525ae760df3b6ef51f860a08
Kurtis Hight <gears2525@aol.com>
2022-03-05 10:34:51 -0800

    fixed deleting gifts

--- client/src/components/RecipientProfile/index.js
+++ client/src/components/RecipientProfile/index.js
@@ -5,86 +5,88 @@ import { useQuery, useMutation } from '@apollo/client';
 import { removeGiftId } from '../../utils/localStorage';
 
 const RecipientProfile = recId => {
-  let recipientID = recId.parentToChild; // No luck with conditional render
+	let recipientID = recId.parentToChild; // No luck with conditional render
+	const { loading, data } = useQuery(GET_RECIPIENT, {
+		variables: {
+			_id: recipientID,
+		},
+	});
 
-  const { loading, data } = useQuery(GET_RECIPIENT, {
-    variables: {
-      _id: recipientID,
-    },
-  });
+	const recipient = data?.recipient || {};
 
-  console.log('data: ', data)
-  const recipient = data?.recipient || {};
-  console.log('recipient: ', recipient);
+	const [removeGift] = useMutation(REMOVE_GIFT);
 
-  const [removeGift] = useMutation(REMOVE_GIFT);
+	const handleDeleteGift = async giftId => {
+		try {
+			const { data } = await removeGift({
+				variables: {
+					recipientId: recipientID,
+					giftId: giftId,
+				},
+			});
+			console.log(data);
+			removeGiftId(giftId);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  const handleDeleteGift = async (giftId) => {
-    try {
-      const { data } = await removeGift({ variables: { 
-        _id: recipient._id, giftId }});
-      removeGiftId(giftId);
-    } catch (err) { console.error(err); }
-  };
+	if (loading) {
+		return <div>Loading</div>;
+	}
 
+	if (!recipient.savedGifts) {
+		return (
+			<p className="bg-dark text-light p-3">
+				{recipient.firstname}, Doesn't have any gifts yet.
+				<a href={`/search&personality=${recipient.traits}&id=${recipient._id}`}>
+					<button className="button is-medium is-warning">Find Gifts!</button>
+				</a>
+			</p>
+		);
+	}
 
-console.log(loading, '\n Line Break', '\n Line Break', recipient);
-if (loading) {
-  return <div>Loading</div>;
-}
-
-if (!recipient.savedGifts) {
-  return (
-    <p className="bg-dark text-light p-3">
-      {recipient.firstname}, Doesn't have any gifts yet.
-      <a href={`/search&personality=${recipient.traits}&id=${recipient._id}`}>
-        <button className="button is-medium is-warning">Find Gifts!</button>
-      </a>
-    </p>
-  );
-}
-
-return (
-  <>
-    <div>{recipient.firstname}</div>
-    <div className="container card-grid results">
-      {recipient.savedGifts.map(giftList => (
-        <div key={giftList.giftId} className="card card-max-height">
-          <div className="card-image">
-            <figure className="image is-half">
-              <img src={giftList.image} alt={giftList.giftId} />
-            </figure>
-          </div>
-          <div className="card-content p-0">
-            <div className="media">
-              <div className="media-content">
-                <p className="title is-4 pl-6"></p>
-              </div>
-            </div>
-            <div className="card-footer">
-              <p className="card-footer-item" style={{ padding: '10px' }}>
-                <button
-                  className="button is-link is-medium"
-                  onClick={() => window.open(giftList.link, '_blank')}
-                >
-                  Buy Now
-                </button>
-              </p>
-              <p className="card-footer-item">
-                <button
-                  className="button is-danger is-medium"
-                  onClick={() => handleDeleteGift(giftList.giftId)}
-                >
-                  Delete Gift
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  </>
-);
+	return (
+		<>
+			<div>{recipient.firstname}</div>
+			<div className="container card-grid results">
+				{recipient.savedGifts.map(giftList => (
+					<div key={giftList.giftId} className="card card-max-height">
+						<div className="card-image">
+							<figure className="image is-half">
+								<img src={giftList.image} alt={giftList.giftId} />
+							</figure>
+						</div>
+						<div className="card-content p-0">
+							<div className="media">
+								<div className="media-content">
+									<p className="title is-4 pl-6"></p>
+								</div>
+							</div>
+							<div className="card-footer">
+								<p className="card-footer-item" style={{ padding: '10px' }}>
+									<button
+										className="button is-link is-medium"
+										onClick={() => window.open(giftList.link, '_blank')}
+									>
+										Buy Now
+									</button>
+								</p>
+								<p className="card-footer-item">
+									<button
+										className="button is-danger is-medium"
+										onClick={() => handleDeleteGift(giftList.giftId)}
+									>
+										Delete Gift
+									</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default RecipientProfile;
