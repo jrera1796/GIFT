08565f16c62722bc8740cb2dfacbc09d2dfb9bb3
Kurtis Hight <gears2525@aol.com>
2022-03-04 23:40:59 -0800

    display saved gifts on dashboard

--- client/src/components/RecipientList/index.js
+++ client/src/components/RecipientList/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import { React, useState } from 'react';
+
 import { useQuery, useMutation } from '@apollo/client';
 import { REMOVE_RECIPIENT } from '../../utils/mutations';
+
 import { GET_USER } from '../../utils/queries';
-import {removeRecipientId} from '../../utils/localStorage';
+import { removeRecipientId } from '../../utils/localStorage';
+import RecipientProfile from '../RecipientProfile';
 
 const RecipientList = ({ username, recipients }) => {
 	const { loading, data } = useQuery(GET_USER);
 	const [removeRecipient, { error }] = useMutation(REMOVE_RECIPIENT);
 	const user = data?.me || {};
-
+	const [data1, setData] = useState('');
 	if (!user.recipients) {
 		return (
 			<p className="bg-dark text-light p-3">
@@ -17,31 +20,34 @@ const RecipientList = ({ username, recipients }) => {
 		);
 	}
 
-	const handleSubmit = async (recipientId) => {
-		console.log(`recipientId ${recipientId}`)
+	const handleSubmit = async recipientId => {
 		try {
-			const { data } = await removeRecipient({ variables: { recipientId }, });
+			const { data } = await removeRecipient({ variables: { recipientId } });
 			console.log(data);
 			removeRecipientId(recipientId);
 		} catch (error) {
 			console.log(error);
 		}
 	};
-	if (loading) {
-		return <h2>Wait, Loading...</h2>
-	  }
 
-	  console.log(user.recipients)
+	const parentToChild = () => {
+	};
+
+	if (loading) {
+		return <h2>Wait, Loading...</h2>;
+	}
+
 	return (
 		<>
 			<div>
 				{user.recipients.map(recArr => (
 					<form className="has-text-weight-bold">
-						<div
-							key={recArr._id}>
+						<div key={recArr._id}>
 							<h6>
-								{recArr.firstname} {recArr.lastname} {recArr._id} {recArr.traits}{' '}
+								{recArr.firstname} {recArr.lastname} {recArr._id}{' '}
+								{recArr.traits}{' '}
 							</h6>
+							<RecipientProfile parentToChild={recArr._id} />
 							<button
 								className="button is-danger"
 								data-testid="button"
--- client/src/components/RecipientProfile/index.js
+++ client/src/components/RecipientProfile/index.js
@@ -3,9 +3,10 @@ import { GET_RECIPIENT } from '../../utils/queries'
 import { useQuery } from '@apollo/client';
 
 
-const RecipientProfile = () => {
+const RecipientProfile = (recId) => {
   let params = (new URL(document.location)).searchParams
-  let recipientID = params.get('id'); // No luck with conditional render
+  let recipientID = recId.parentToChild; // No luck with conditional render
+ 
   const { loading, data } = useQuery(GET_RECIPIENT, {
     variables: {
       _id: recipientID
