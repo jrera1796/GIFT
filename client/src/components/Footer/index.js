2b9553c9972bfe72c600e3e443c4570aa44d0b38
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-03-05 20:29:00 -0800

    Styling changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,13 +3,47 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">
-        &copy;{new Date().getFullYear()} by
-        <a href="https://github.com/kcheykim" className='has-text-white'> Kosal Cheykim</a>,
-        <a href="https://github.com/mockcomic"className='has-text-white'> Kurtis Hight</a>,
-        <a href="https://github.com/jrera1796"className='has-text-white'> Jose Rivera</a>,
-        <a href="https://github.com/ricky0320"className='has-text-white'> Ching Leung</a>
-      </div>
+      <div className="container columns">
+        <div className='column has-text-white'>
+          &copy;{new Date().getFullYear()}
+        </div>
+        <div className='column'>
+
+          <a href="https://github.com/kcheykim" target="_blank" rel='noreferrer' noopener><button
+            className="button btn is-info"
+            data-testid="button"
+            type="submit"
+          >
+            Kosal Cheykim
+          </button></a>
+
+          <a href="https://github.com/mockcomic" target="_blank" rel='noreferrer' noopener><button
+            className="button btn is-info"
+            data-testid="button"
+            type="submit"
+          >
+            Kurtis Hight
+          </button></a>
+
+          <a href="https://github.com/jrera1796" target="_blank" rel='noreferrer' noopener><button
+            className="button btn is-info"
+            data-testid="button"
+            type="submit"
+          >
+            Jose Rivera
+          </button></a>
+
+          <a href="https://github.com/ricky0320" target="_blank" rel='noreferrer' noopener><button
+            className="button btn is-info"
+            data-testid="button"
+            type="submit"
+          >
+            Ching Leung
+          </button></a>
+
+          </div>
+        </div>
+
     </footer>
   );
 };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,9 +9,9 @@ export default function Login() {
 	<div className='columns mt-3 is-full is-centered'>
 		{toggle?(
 
-		<div className='box has-background-dark is-full'><h1 className='has-text-white'>Log In Below! Don't have account yet? Click Here!</h1><button className='is-button' onClick={() => setToggle(false)}>SignUp</button><LoginForm /></div>) 
+		<div className='box has-background-dark is-full'><h1 className='has-text-white'>Log In Below! Don't have account yet? Click Here!</h1><button className='is-button is-size-4 btn' onClick={() => setToggle(false)}>SignUp</button><LoginForm /></div>) 
 		:(
-			<div className='box has-background-dark is-full'><h1 className='has-text-white'>Sign Up Below! Already have an account? Click here!</h1><button className='is-button' onClick={() => setToggle(true)}>Login</button><SignupForm/></div>
+			<div className='box has-background-dark is-full'><h1 className='has-text-white'>Sign Up Below! Already have an account? Click here!</h1><button className='is-button is-size-4 btn' onClick={() => setToggle(true)}>Login</button><SignupForm/></div>
 		)
 		}
 	</div>
