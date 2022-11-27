94844cc3dd2ecfda5726bf71a6a14d465f04ea8c
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-11-26 23:42:21 -0800

    fixing graphql on vercel

--- server/server.js
+++ server/server.js
@@ -17,6 +17,20 @@ const startServer = async () => {
   });
   await server.start();
   server.applyMiddleware({ app });
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+ 
+   if (req.method === "OPTIONS") {
+         return res.sendStatus(200);
+   }
+   next();
+  });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
